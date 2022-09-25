3302f1b4eb83b273c6ff512580eecb41e872831c
coolxer <lukasz.milos@int.pl>
2022-09-25 15:21:01 +0200

    add own palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./resources/**/*.blade.php"],
+    content: ['./resources/**/*.blade.php'],
     theme: {
+        colors: {
+            dominant: '#ebf2fa',
+            secondary: '#292f36',
+            accent: '#23ce6b',
+        },
         extend: {},
     },
     plugins: [],
